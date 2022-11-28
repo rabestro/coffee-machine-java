40d34d781e0a1813623d1c4ea4aaf46baf73c7a0
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-28 16:52:24 +0200

    update the specification for storage block replenishment

--- src/test/groovy/machine/unit/StorageBlockFillSpec.groovy
+++ src/test/groovy/machine/unit/StorageBlockFillSpec.groovy
@@ -1,29 +1,42 @@
 package machine.unit
 
 import machine.model.Resource
+import spock.lang.Issue
+import spock.lang.Narrative
 import spock.lang.PendingFeature
+import spock.lang.See
 import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Title
 
+@Title('The technician replenishes the resources in the storage unit')
+@Narrative('''
+As a technician
+I want to replenish the necessary resources in the coffee machine
+So that the coffee machine can prepare the necessary coffee drinks
+''')
+@See('https://github.com/rabestro/coffee-machine/wiki/Resources-replenishment')
+@Issue('4')
+@Subject(StorageBlock)
 class StorageBlockFillSpec extends Specification {
 
     @PendingFeature
     def 'replenish resource for empty storage block'() {
 
-        given:
+        given: 'a storage block'
         def storageUnit = new StorageBlock()
 
-        expect:
+        expect: 'the storage block has not resources'
         storageUnit.volume(resource) == 0
 
-        when:
+        when: 'we refill the resource'
         storageUnit.fill resource, replenishment
 
-        then:
-        storageUnit.volume(resource) == expectedVolume
+        then: 'the volume of this resource is equal to replenishment'
+        storageUnit.volume(resource) == replenishment
 
-        where:
-        resource = Resource.WATER
-        replenishment = 5000
-        expectedVolume = replenishment
+        where: 'available resources and replenishment'
+        resource << Resource.values()
+        replenishment = 100
     }
 }
