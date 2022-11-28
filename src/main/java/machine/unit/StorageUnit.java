b375d858cd73c030b6372837d6558dc417dcc53c
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-28 12:43:26 +0200

    add an interface for StorageUnit

---
+++ src/main/java/machine/unit/StorageUnit.java
@@ -0,0 +1,8 @@
+package machine.unit;
+
+import machine.model.Resource;
+
+public interface StorageUnit {
+    int volume(Resource resource);
+    void fill(Resource resource, int replenishment);
+}
