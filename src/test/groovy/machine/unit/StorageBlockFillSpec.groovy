a960a4f35d3a97570b8ffe3306358bae618626f1
Jegors Čemisovs <jegors_cemisovs@epam.com>
2022-11-30 14:22:21 +0200

    updated StorageUnitFillSpec

--- src/test/groovy/machine/unit/StorageBlockFillSpec.groovy
+++ src/test/groovy/machine/unit/StorageBlockFillSpec.groovy
@@ -8,58 +8,60 @@ import java.util.concurrent.ThreadLocalRandom
 @Title('The technician replenishes the resources in the storage unit')
 @Narrative('''
 As a technician
-I want to replenish the necessary resources in the coffee machine
-So that the coffee machine can prepare the necessary coffee drinks
+I want to replenish the resources in the storage unit
+So that the coffee machine can prepare the coffee drinks
 ''')
 @See('https://github.com/rabestro/coffee-machine/wiki/Resources-replenishment')
 @Issue('4')
 @Subject(StorageBlock)
+@Tag('Sanity')
 class StorageBlockFillSpec extends Specification {
 
-    def 'replenish resource for an empty storage unit'() {
+    def 'replenish resource for an empty storage unit'(Resource resource) {
 
         given: 'an empty storage unit'
         def storageUnit = new StorageBlock()
 
-        expect: 'the storage unit has not resources'
+        expect: 'the storage unit is missing a specific resource'
         storageUnit.volume(resource) == 0
 
-        when: 'we refill the resource'
+        when: 'we replenish the specific resource'
         storageUnit.fill resource, replenishment
 
-        then: 'the volume of this resource is equal to replenishment'
+        then: 'the volume of the resource is equal to replenishment'
         storageUnit.volume(resource) == replenishment
 
-        where: 'available resources and replenishment'
+        where: 'all available resources and their replenishment'
         resource << Resource.values()
-        replenishment = 'random resource volume'()
+        replenishment = 'some resource volume'()
     }
 
     def 'replenish resource for a filled storage unit'(Resource resource) {
 
-        given: 'a filled storage unit'
+        given: 'storage unit with a certain amount of all resources'
         def storageUnit = 'filled storage unit'()
 
-        when: 'we refill the resource'
+        when: 'we replenish a specific resource in storage unit'
         storageUnit.fill resource, replenishment
 
-        then: 'the volume of this resource is equal to replenishment'
+        then: 'the volume of the resource increased by replenishment'
         storageUnit.volume(resource) == old(storageUnit.volume(resource)) + replenishment
 
-        where: 'available resources and replenishment'
+        where: 'all available resources and their replenishment'
         resource << Resource.values()
-        replenishment = 'random resource volume'()
+        replenishment = 'some resource volume'()
     }
 
     def 'filled storage unit'() {
         def storage = new StorageBlock()
         Resource.values().every {
-            storage.fill it, 'random resource volume'()
+            storage.fill it, 'some resource volume'()
         }
         return storage
     }
 
-    def 'random resource volume'() {
-        ThreadLocalRandom.current().nextInt(1, 5000)
+    def 'some resource volume'() {
+        ThreadLocalRandom.current().nextInt(50) * 10
     }
 }
+
